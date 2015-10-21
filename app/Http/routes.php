4d01cf114cf49e88a283a10e15bc9fb33ce7a50a
oscar arocha <arocha.oscar@gmail.com>
2015-10-21 11:07:43 -0430

    agrenado inicio de sesion

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Authentication routes...
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
